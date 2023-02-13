d95e7f588d5ccd06af99dd66a2576e12b059a721
Joshua Kawamata <email>
2023-02-13 16:56:09 +0900

    test: react redux test components

--- src/DemoCounter.test.tsx
+++ src/DemoCounter.test.tsx
@@ -1,9 +1,26 @@
+import { AnyAction, configureStore, Store } from '@reduxjs/toolkit';
 import { render, screen, fireEvent } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import counterReducer from './features/counter/counterSlice';
+
 import { DemoCounter } from './DemoCounter';
 
-describe('render Counter', () => {
+describe('render DemoCounter', () => {
+    let store: Store<unknown, AnyAction>;
+    beforeEach(() => {
+        store = configureStore({
+            reducer: {
+                counter: counterReducer,
+            },
+        });
+    });
+
     it('should count by button press', () => {
-        render(<DemoCounter />);
+        render(
+            <Provider store={store}>
+                <DemoCounter />
+            </Provider>
+        );
 
         expect(screen.getByTestId('count-element')).toHaveTextContent('0');
         fireEvent.click(
