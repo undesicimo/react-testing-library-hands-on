1d391bb3d12da2b92db098f65af57b286bfc5d6a
Joshua Kawamata <email>
2023-02-13 16:36:59 +0900

    test: test for increment and decrement action

--- src/DemoCounter.test.tsx
+++ src/DemoCounter.test.tsx
@@ -1,11 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { DemoCounter } from './DemoCounter';
-describe('render DemoCounter', () => {
-    it('should render all elements', () => {
+
+describe('render Counter', () => {
+    it('should count by button press', () => {
         render(<DemoCounter />);
 
-        expect(screen.getByText('DemoCounter')).toBeInTheDocument();
-        expect(screen.getByRole('textbox')).toBeInTheDocument();
-        expect(screen.getAllByRole('button')).toHaveLength(2);
+        expect(screen.getByTestId('count-element')).toHaveTextContent('0');
+        fireEvent.click(
+            screen.getByRole('button', { name: 'Increment value' })
+        );
+        fireEvent.click(
+            screen.getByRole('button', { name: 'Increment value' })
+        );
+        expect(screen.getByTestId('count-element')).toHaveTextContent('2');
+        fireEvent.click(
+            screen.getByRole('button', { name: 'Decrement value' })
+        );
+        expect(screen.getByTestId('count-element')).toHaveTextContent('1');
     });
 });
