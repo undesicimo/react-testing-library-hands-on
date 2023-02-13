e5e48fac84e637f82552f610373f184c988ecbb6
Joshua Kawamata <email>
2023-02-13 16:36:30 +0900

    feat: added hook functionallity

--- src/DemoCounter.tsx
+++ src/DemoCounter.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from 'react';
+import styles from './Counter.module.css';
 
 export const DemoCounter = () => {
-  return (
-    <div>
-      <div>DemoCounter</div>
-      <input type="text" />
-      <button>Click1</button>
-      <button>Click2</button>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            <div className={styles.row}>
+                <button
+                    className={styles.button}
+                    aria-label='Decrement value'
+                    onClick={() => setCount(current => current - 1)}>
+                    -
+                </button>
+                <span
+                    className={styles.value}
+                    data-testid='count-element'>
+                    {count}
+                </span>
+                <button
+                    className={styles.button}
+                    aria-label='Increment value'
+                    onClick={() => setCount(current => current + 1)}>
+                    +
+                </button>
+            </div>
+        </div>
+    );
 };
-
